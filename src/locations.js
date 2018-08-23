6d7b70c105e321633e69603c9347c0cf94795db7
Mohaned-Adel <mohaned.adel1996@gmail.com>
2018-08-23 20:20:22 +0200

    add the locations and styles

---
+++ src/locations.js
@@ -0,0 +1,41 @@
+export const Locations = [{
+    location: {
+      lat: 31.208874,
+      lng: 29.909184
+    },
+    title: 'Bibliotheca Alexandrina',
+    address: 'El-Gaish Rd, Al Azaritah WA Ash Shatebi, Qesm Bab Sharqi, Alexandria Governorate, Egypt',
+  },
+  {
+    location: {
+      lat: 31.213251,
+      lng: 29.885194
+    },
+    title: 'Qaitbay Maritime Museum',
+    address: 'Quaitbay Fort, As Sayalah Sharq, Qesm Al Gomrok, Alexandria Governorate, Egypt'
+  },
+  {
+    location: {
+      lat: 31.212829,
+      lng: 29.885001
+    },
+    title: 'Alexandria Aquarium',
+    address: '1 Kayetbai, As Sayalah Sharq, Qesm Al Gomrok, Alexandria Governorate, Egypt'
+  },
+  {
+    location: {
+      lat: 31.200009,
+      lng: 29.893713
+    },
+    title: 'El Gondy El Maghool Square',
+    address: 'Al Gonday Al Maghool Sq., Al Mansheyah Al Kubra, Qism El-Mansheya, Alexandria Governorate, Egypt'
+  },
+  {
+    location: {
+      lat: 31.207287,
+      lng: 29.924172
+    },
+    title: 'Faculty of Engineering - Alexandria University',
+    address: 'Faculty Of Engineering, Al Azaritah WA Ash Shatebi, Qesm Bab Sharqi, Alexandria Governorate, Egypt'
+  },
+  ];
